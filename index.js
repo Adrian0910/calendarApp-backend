a116760e60a41e005eaff2407ede976e7a260917
Adrian0910 <uchihasantander@gmail.com>
2021-09-15 14:18:55 -0500

    express routes, port and env conf added

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 
-console.log('Hola mundo')
+const express = require('express');
+require('dotenv').config();
+
+console.log( process.env );
+// Create express server
+const app = express();
+
+
+// Public dir
+app.use( express.static('public') );
+
+//Routes
+/* app.get('/', (req, res) => {
+
+    res.json({
+        ok: true
+    })
+}); */
+
+// listener
+app.listen( process.env.PORT, () => {
+    console.log(`Server running port ${ process.env.PORT }`);
+});
 
 
