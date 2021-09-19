031b3544f2f8d900004ebe7af90b5676243e9c7a
Adrian0910 <uchihasantander@gmail.com>
2021-09-18 22:29:36 -0500

    conf mongo

--- controllers/events.js
+++ controllers/events.js
@@ -22,7 +22,7 @@ const createEvent = async(req, res = response) => {
         event.user = req.uid;
         const eventSaveDB = await event.save();
 
-        res.json({
+        return res.json({
             ok: true,
             event: eventSaveDB
         })
@@ -65,7 +65,7 @@ const updateEvent = async(req, res = response) => {
 
         const eventUpdate = await Event.findByIdAndUpdate( eventId, newEvent, { new: true} );
 
-        res.json({
+        return res.json({
             ok: true,
             event: eventUpdate
         });
