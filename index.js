212495ee2dbf53d5af8ae6df57444d1813e369d8
Adrian0910 <uchihasantander@gmail.com>
2021-09-19 22:24:55 -0500

    Version 1.2.0 de Calendar

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use( express.static('public') );
 app.use( express.json() );
 
 //Routes
+app.use('/login', express.static('public'));
 app.use('/api/auth', require('./routes/auth') );
 app.use('/api/events', require('./routes/events') );
 
